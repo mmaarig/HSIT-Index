a431651a82c59ed3d845e08c05d2d5feb246dab9
Muhammad Maarig <mmaarig@gmail.com>
2024-09-27 00:09:12 +0500

    Assignments 1 & 2 Done

---
+++ Assignment 2/app.js
@@ -0,0 +1,29 @@
+let username;
+let myName = "Muhammad Maarig";
+// alert(myName);
+
+let message = "Hello World";
+alert(message);
+
+let bio = "John Doe\n15 Years Old\nCertified Mobile Application Developer";
+alert(bio);
+
+let pizza = "Pizza\nPizz\nPiz\nPi\nP";
+alert(pizza);
+
+let email = "example@example.com";
+alert("My email address is "+ email);
+
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book "+ book);
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+let symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbol);
+
+
+// alert("Error! Please enter a valid password");
+// alert("Welcome to JS Land...\nHappy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
