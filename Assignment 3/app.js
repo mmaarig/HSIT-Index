b9e9775782c82a0342f0f0b633b4c2714993d62f
Muhammad Maarig <81269436+mmaarig@users.noreply.github.com>
2024-10-05 12:33:40 +0500

    Update app.js

--- Assignment 3/app.js
+++ Assignment 3/app.js
@@ -25,35 +25,3 @@ document.write("My birth year is " + birthYear + ". Data type of my declared var
 let visitorName = "John Doe", product = "T-Shirt(s)", qty = 5;
 
 document.write("<br><br>"+visitorName+" ordered "+ qty + " "+product+" on XYZ Clothing store");
-
-
-
-// let username;
-// let myName = "Muhammad Maarig";
-// // alert(myName);
-
-// let message = "Hello World";
-// alert(message);
-
-// let bio = "John Doe\n15 Years Old\nCertified Mobile Application Developer";
-// alert(bio);
-
-// let pizza = "Pizza\nPizz\nPiz\nPi\nP";
-// alert(pizza);
-
-// let email = "example@example.com";
-// alert("My email address is "+ email);
-
-// let book = "A smarter way to learn JavaScript";
-// alert("I am trying to learn from the Book "+ book);
-
-// document.write("Yah! I can write HTML content through JavaScript");
-
-// let symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
-// alert(symbol);
-
-
-// alert("Error! Please enter a valid password");
-// alert("Welcome to JS Land...\nHappy Coding!");
-// alert("Welcome to JS Land...");
-// alert("Happy Coding!");
